7bf1d7ffaa272ebb406e8a1c8a072ffb9e518d2b
İHLASMERT <ihlas1mert@gmail.com>
2023-01-21 14:47:52 +0300

    added use state basic exmaple

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Example from './useState/useSatetBasics'
+
+function App() {
+  return (
+    <div className='container'>
+      <h2>Advanced React</h2>
+      <Example></Example>
+    </div>
+  )
+}
+export default App
